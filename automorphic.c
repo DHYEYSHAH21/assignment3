456bac5eb6c85e45589bfe09c6b7872a93783fb8
DHYEYSHAH21 <dhyeyshah212007@gmail.com>
2025-09-03 10:31:59 -0400

    Create automorphic.c

---
+++ automorphic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num, square, temp, digits = 0, lastDigits;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Calculate square of the number
+    square = num * num;
+
+    // Count number of digits in the number
+    temp = num;
+    while(temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    // Get last 'digits' digits of the square
+    lastDigits = square % (int)pow(10, digits);
+
+    // Check if last digits of square equal the number
+    if(lastDigits == num)
+        printf("%d is an Automorphic number.\n", num);
+    else
+        printf("%d is NOT an Automorphic number.\n", num);
+
+    return 0;
+}
