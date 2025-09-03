d91c0ff2a4d3b99cbe8986b6393d858d4b41bd01
DHYEYSHAH21 <dhyeyshah212007@gmail.com>
2025-09-03 10:20:20 -0400

    Create find summation of prime nos between 1 and 500.c

---
+++ find summation of prime nos between 1 and 500.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int sum = 0;
+    int isPrime;
+
+    // Loop through numbers from 2 to 500
+    for(i = 2; i <= 500; i++) {
+        isPrime = 1; // Assume i is prime
+
+        // Check if i is divisible by any number from 2 to sqrt(i)
+        for(j = 2; j*j <= i; j++) {
+            if(i % j == 0) {
+                isPrime = 0; // Not prime
+                break;
+            }
+        }
+
+        if(isPrime) {
+            sum += i; // Add prime to sum
+        }
+    }
+
+    printf("Sum of prime numbers between 1 and 500 is: %d\n", sum);
+    return 0;
+}
